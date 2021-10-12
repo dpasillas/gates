4ca19534e9f86d5334d2582502fe1455cb57a892
Daniel Pasillas <cs.daniel.pasillas@gmail.com>
2021-10-12 02:21:28 -0700

    Materialized Sidepanel Permanent Side panel with vertical tabs Light and dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
+import 'typeface-roboto';
 import 'typeface-roboto-mono';
 import reportWebVitals from './reportWebVitals';
 import App from './components/App';
+import {StyledEngineProvider} from "@mui/styled-engine";
 
 ReactDOM.render(
     <React.StrictMode>
-      <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
+      <StyledEngineProvider>
+        <App/>
+      </StyledEngineProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
